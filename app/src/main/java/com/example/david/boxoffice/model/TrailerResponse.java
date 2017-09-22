507e3a98d832361bf62e67e6ad7016739ca42209
David Kahara <kaharadave@gmail.com>
2017-09-22 19:14:36 +0300

    Added the trailer response class

--- app/src/main/java/com/example/david/boxoffice/model/TrailerResponse.java
+++ app/src/main/java/com/example/david/boxoffice/model/TrailerResponse.java
@@ -1,8 +1,29 @@
 package com.example.david.boxoffice.model;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 /**
  * Created by david on 9/22/17.
  */
 
 public class TrailerResponse {
+
+    @SerializedName("id")
+    private int id_trailer;
+    @SerializedName("results")
+    private List<Trailer> results;
+
+    public int getIdTrailer(){
+        return id_trailer;
+    }
+
+    public void seIdTrailer(int id_trailer){
+        this.id_trailer = id_trailer;
+    }
+
+    public List<Trailer> getResults(){
+        return results;
+    }
 }
