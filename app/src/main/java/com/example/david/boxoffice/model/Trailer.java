84a3e62ca5c40308b4eb4aa93ca9928f5b0a4326
David Kahara <kaharadave@gmail.com>
2017-09-22 17:55:39 +0300

    Created a model for the Trailer objects

---
+++ app/src/main/java/com/example/david/boxoffice/model/Trailer.java
@@ -0,0 +1,34 @@
+package com.example.david.boxoffice.model;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by david on 9/22/17.
+ */
+public class Trailer {
+
+    @SerializedName("key")
+    private String key;
+    @SerializedName("name")
+    private String name;
+
+    public Trailer(String key, String name){
+        this.key = key;
+        this.name = name;
+    }
+
+    public String getKey(){
+        return key;
+    }
+
+    public void setKey(String key){
+        this.key = key;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
